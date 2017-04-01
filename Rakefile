9af4b73aa48675c32851cca402a6dc0bbe233a29
Wolfgang Teuber <wolfgang.teuber@sage.de>
2017-04-01 23:19:32 +0200

    Monkey-patch Rake::Application to support YARD::Rake::YardocTask

--- Rakefile
+++ Rakefile
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require './tmpfix/rake_application'
+
 require 'bundler/gem_tasks'
+
 require 'rspec/core/rake_task'
 require 'rubocop/rake_task'
 require 'yard'
---
+++ tmpfix/rake_application.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Remove this file together with its require statements  as soon as
+# YARD::Rake::YardocTask stops depending on last_comment.
+# last_comment has been removed in Rake 11.
+module TempFixForRakeLastComment
+  def last_comment
+    last_description
+  end
+end
+Rake::Application.send :include, TempFixForRakeLastComment

44453db8218d68c67d55a94f792fb7bf1f622aba
Wolfgang Teuber <wolfgang.teuber@sage.de>
2017-05-01 00:46:40 +0200

    Add documentation to files outside YamlNormalizer namespace

--- tmpfix/rake_application.rb
+++ tmpfix/rake_application.rb
@@ -4,8 +4,10 @@
 # YARD::Rake::YardocTask stops depending on last_comment.
 # last_comment has been removed in Rake 11.
 module TempFixForRakeLastComment
+  # Track the last comment made in the Rakefile.
   def last_comment
     last_description
   end
 end
+
 Rake::Application.send :include, TempFixForRakeLastComment

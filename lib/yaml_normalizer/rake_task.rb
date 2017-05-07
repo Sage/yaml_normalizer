31388c39c336407c6182a31038ffbffaa29ba605
Wolfgang Teuber <wolfgang.teuber@sage.de>
2017-05-07 10:34:42 +0200

    Fixes #12: Add file configuration to rake task

--- lib/yaml_normalizer/rake_task.rb
+++ lib/yaml_normalizer/rake_task.rb
@@ -1,35 +1,54 @@
 # frozen_string_literal: true
 
+lib = File.expand_path(File.join('..', '..', '..', 'lib'), __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'yaml_normalizer'
 
 module YamlNormalizer
   # Provides Rake task integration
   class RakeTask < ::Rake::TaskLib
+    # The name of the task
     # @return [String] name of the Rake task
     attr_accessor :name
     # @return [Array] arguments to be passed to Suggest.run
     attr_accessor :args
+
+    # The YAML files to process.
+    # @example Task files assignment
+    #   YamlNormalizer::RakeTask.new do |task|
+    #     task.files = ['config/locale/*.yml', 'config/*.yml']
+    #   end
+    # @return [Array<String>] a list of file globing Strings
+    attr_accessor :files
 
     # Create a YamlNormalizer rake task object.
     # Use this to
     # @example
     #   In your Rakefile, add:
     #     YamlNormalizer::RakeTask.new
+    #
+    #   To be more specific, configure YAML Normalizer's mode and files like so:
+    #     YamlNormalizer::RakeTask.new do |config|
+    #       config.files = Dir[File.join(File.dirname(__FILE__), 'include.yml')]
+    #     end
+    #
+    #   This gives you the following tasks (run rake -T)
+    #     rake yaml:normalize        # Normalize given YAML files
     # @param name [String] name of the Rake task
-    # @param *args [Array] arguments to be passed to Normalize.call
     # @param &block [Proc] optional, evaluated inside the task definition
-    def initialize(name = 'yaml_normalizer', *args, &block)
-      @name = name
-      @args = args
+    def initialize(name = 'yaml', &block)
       yield(self) if block
 
-      desc 'Normalize all YAML files in path'
-      task(@name) { normalize }
+      desc 'Normalize configured YAML files'
+      task("#{name}:normalize") { normalize }
     end
 
-    # @return [void]
+    private
+    end
+
+    # Normalizes configured YAML files
     def normalize
-      ::YamlNormalizer::Services::Normalize.call('**/*')
+      Services::Normalize.call(*files)
     end
   end
 end

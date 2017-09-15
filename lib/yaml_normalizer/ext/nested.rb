11810e513ad50d03c6216730089fe917e283221f
Wolfgang Teuber <wolfgang.teuber@sage.de>
2017-09-15 04:50:24 +0200

    improve documentation of YamlNormalizer::Ext namespace

--- lib/yaml_normalizer/ext/nested.rb
+++ lib/yaml_normalizer/ext/nested.rb
@@ -2,15 +2,18 @@
 
 module YamlNormalizer
   module Ext
-    # Extends an instance of Hash to add the method nested.
+    # *YamlNormalizer::Ext::Nested* extends instances of *Hash* to provide the
+    # additional public helper method *nested*.
     # The approach of extending Hash instances avoids monkey-patching a Ruby
     # Core class and using refinements.
     module Nested
-      # Transforms a flat key-value pair Hash into a tree-shaped Hash, assuming
-      # tree levels are separated by a dot. nested does not modify the Hash
-      # it's called on.
+      # Transforms a flat key-value pair *Hash* into a tree-shaped *Hash*,
+      # assuming tree levels are separated by a dot.
+      # *nested* does not modify the instance of *Hash* it's called on.
       # @example
-      #   {'a.b.c' => 1, 'b.x' => 2, 'b.y.ok' => true, 'b.z' => 4}.nested
+      #   hash = {'a.b.c' => 1, 'b.x' => 2, 'b.y.ok' => true, 'b.z' => 4}
+      #   hash.extend(YamlNormalizer::Ext::Nested)
+      #   hash.nested
       #   => {"a"=>{"b"=>{"c"=>1}}, "b"=>{"x"=>2, "y"=>{"ok"=>true}, "z"=>4}}
       # @return [Hash] tree-shaped Hash
       def nested
